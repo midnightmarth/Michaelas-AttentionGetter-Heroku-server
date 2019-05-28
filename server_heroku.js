76dfeecb79de717cf9921d8a743f5978c845d35d
AtEchoOff <midnightmarth@gmail.com>
2019-05-28 01:55:51 -0500

    Switched from using express-ws to Socket.io

--- server_heroku.js
+++ server_heroku.js
@@ -1,43 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const expressWs = require("express-ws")(app);
-// const cors = require("cors");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// app.use(cors);
-
-let holder;
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/api/_healthcheck", (req, res) => {
   res.send({ status: "Ok" });
 });
 
-app.ws("/api/ws", (req, res, next) => {
-  // if (holder) {
-  //   ws.send(holder);
-  //   console.log("notifying serverM");
-  //   holder = undefined;
-  // }
-
-  app.on("message", () => {
-    console.log("All arguments when ws hit: \n", ...arguments);
+io.on("connection", socket => {
+  console.log("Connection was made");
+  socket.on("button_press", message => {
+    console.log("Button press was activated");
+    switch (message.status) {
+      case "on":
+        io.emit("turn on");
+        break;
+      case "off":
+        io.emit("turn off");
+        break;
+    }
   });
-  app.on("close", () => {
-    console.log("Closed connection");
-  });
-  holder = ws.send;
-  ws.send({ status: "Hello There!" });
 });
 
-app.post("/api/button_press", (req, res) => {
-  console.log(holder);
-  holder(req.body);
-  res.end();
-});
-
-var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+http.listen(8080, function() {
+  console.log("App now running on port 8080");
 });
